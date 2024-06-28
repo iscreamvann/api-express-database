211ca99ab4b32c2ee9ac9fbe9ceb05d4268319f2
Kyle Vann <kyle.vann1996@icloud.com>
2024-06-28 21:02:18 +0100

    couple more requests

--- src/routers/books.js
+++ src/routers/books.js
@@ -6,5 +6,9 @@ router.get('/', async (req, res) => {
     const response = await db.query("select * FROM books", [])
     res.json({response})
 })
+router.get('/:id', async (req, res) => {
+    const response = await db.query("select * FROM books WHERE id = ? ", [req.params.id])
+    res.json({response})
+})
 
 module.exports = router
