7a3d69d0e633f06566d814cf3fbcc1fff81249c5
Kyle Vann <kyle.vann1996@icloud.com>
2024-06-30 14:41:48 +0100

    need to gwet tests passing

--- src/routers/books.js
+++ src/routers/books.js
@@ -10,5 +10,35 @@ router.get('/:id', async (req, res) => {
     const response = await db.query("select * FROM books WHERE id = ? ", [req.params.id])
     res.json({books: response})
 })
+router.put('/:id', async (req, res) => {
+    console.log("BumbaClap", req.body)
+    const update = await db.query(
+        `UPDATE books
+            SET title = ?,
+            type = ?,
+            author = ?,
+            topic = ?,
+            publication_date = ?,
+            pages = ?
+            WHERE id = ?;
+`, [req.body.title, req.body.type, req.body.author, req.body.topic, req.body.publication_date, req.body.pages, req.params.id ])
+    const response = await db.query("select * FROM books WHERE id = ? ", [req.params.id])
+    res.json({books: response})
+})
+router.delete('/:id', async (req, res) => {
+    const response = await db.query("select * FROM books WHERE id = ? ", [req.params.id])
+    await db.query("DELETE FROM books WHERE id = ?", [req.params.id])
+    res.json({books: response})
+})
+router.post('/', async (req, res) => {
+    console.log("BumbaClap", req.body)
+    const update = await db.query(
+        `INSERT INTO books (title, type, author, topic, publication_date, pages)
+VALUES (?, ?, ?, ?, ?, ?);
+`, [req.body.title, req.body.type, req.body.author, req.body.topic, req.body.publication_date, req.body.pages])
+    console.log("test", update.insertId)
+const response = await db.query("select * FROM books WHERE id = ? ", [update.insertId])
+    res.json({books: response})
+})
 
 module.exports = router
